0f4abb930127c9f8ad5e25626fbb0ea72b426f90
Ved Prakash <vedprakashsigh@gmail.com>
2023-05-30 12:47:23 +0530

    .

--- flask/main.py
+++ flask/main.py
@@ -1,13 +1,22 @@
 from flask import Flask
+import urllib.request, json
+from dotenv import load_dotenv
+import os
 
 
+load_dotenv()
 app = Flask(__name__)
+API_URL = os.getenv("API_URL")
 
 
-@app.route("/")
-def home():
-    return "<h1>Hello World</h1>"
+@app.route("/api/get_movies")
+def get_movies():
+    response = urllib.request.urlopen(API_URL)
+    data = response.read()
+    movie_list = json.loads(data)
+    return movie_list["results"][0]
 
 
 if __name__ == "__main__":
+    app.secret_key = os.getenv("SECRET_KEY")
     app.run(debug=True)
--- react/src/components/Content.js
+++ react/src/components/Content.js
@@ -1,15 +1,26 @@
-import { useTMDB } from "../useTMDB";
+// import { useTMDB } from "../useTMDB";
+import Axios from "axios";
 import { InfoCard } from "./InfoCard";
 import { Pagenation } from "./Pagenation";
 
-const API_KEY = "api_key=e09e73373fe71e5e88a2defeacfd7210";
-const BASE_URL = "https://api.themoviedb.org/3";
-const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
-const SEARCH_URL = BASE_URL + "/search/movie?" + API_KEY;
-const IMG_URL = "https://image.tmdb.org/t/p/w500";
+import { useEffect, useState } from "react";
+
+// const API_KEY = "api_key=e09e73373fe71e5e88a2defeacfd7210";
+// const BASE_URL = "https://api.themoviedb.org/3";
+// const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
+// const SEARCH_URL = BASE_URL + "/search/movie?" + API_KEY;
+// const IMG_URL = "https://image.tmdb.org/t/p/w500";
 
 export const Content = () => {
-  const [movieList, refetch] = useTMDB(API_URL);
+  const [movieList, setMovieList] = useState({});
+
+  useEffect(() => {
+    setMovieList(() => {
+      return Axios.get("http://127.0.0.1:5000/api/get_movies").then(
+        (res) => res.data
+      );
+    });
+  }, []);
 
   return (
     <main className="main-content">
