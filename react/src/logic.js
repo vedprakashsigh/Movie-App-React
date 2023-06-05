178cd9448f0d77bad89ba68133ba764aa349472b
Ved Prakash <vedprakashsigh@gmail.com>
2023-06-05 16:36:18 +0530

    Deployment Ready

--- react/src/logic.js
+++ react/src/logic.js
@@ -7,12 +7,12 @@ export const getMovies = async (
   pgno = 1
 ) => {
   if (pgno === 1) {
-    return await Axios.get(`http://127.0.0.1:5000/api/get_movies/${pgno}`).then(
-      (res) => {
-        setMoviesShows(res.data.results);
-        setLastUrl(`http://127.0.0.1:5000/api/get_movies`);
-      }
-    );
+    return await Axios.get(
+      `https://flask-app-movie-app.onrender.com/api/get_movies/${pgno}`
+    ).then((res) => {
+      setMoviesShows(res.data.results);
+      setLastUrl(`https://flask-app-movie-app.onrender.com/api/get_movies`);
+    });
   } else {
     return await Axios.get(`${lastUrl}/${pgno}`).then((res) => {
       setMoviesShows(res.data.results);
@@ -28,10 +28,10 @@ export const getTVShows = async (
 ) => {
   if (pgno === 1) {
     return await Axios.get(
-      `http://127.0.0.1:5000/api/get_tv_shows/${pgno}`
+      `https://flask-app-movie-app.onrender.com/api/get_tv_shows/${pgno}`
     ).then((res) => {
       setMoviesShows(res.data.results);
-      setLastUrl(`http://127.0.0.1:5000/api/get_tv_shows/`);
+      setLastUrl(`https://flask-app-movie-app.onrender.com/api/get_tv_shows/`);
     });
   } else {
     return await Axios.get(`${lastUrl}/${pgno}`).then((res) => {
@@ -50,10 +50,12 @@ export const search_movies = async (
   if (query === "") return getMovies(setMoviesShows, setLastUrl, lastUrl);
   if (pgno === 1) {
     return await Axios.get(
-      `http://127.0.0.1:5000/api/movie_search/${query}/${pgno}`
+      `https://flask-app-movie-app.onrender.com/api/movie_search/${query}/${pgno}`
     ).then((res) => {
       setMoviesShows(res.data.results);
-      setLastUrl(`http://127.0.0.1:5000/api/movie_search/${query}`);
+      setLastUrl(
+        `https://flask-app-movie-app.onrender.com/api/movie_search/${query}`
+      );
     });
   } else {
     return await Axios.get(`${lastUrl}/${pgno}`).then((res) => {
@@ -72,10 +74,12 @@ export const search_tv = async (
   if (query === "") return getTVShows(setMoviesShows, setLastUrl, lastUrl);
   if (pgno === 1) {
     return await Axios.get(
-      `http://127.0.0.1:5000/api/tv_search/${query}/${pgno}`
+      `https://flask-app-movie-app.onrender.com/api/tv_search/${query}/${pgno}`
     ).then((res) => {
       setMoviesShows(res.data.results);
-      setLastUrl(`http://127.0.0.1:5000/api/tv_search/${query}/`);
+      setLastUrl(
+        `https://flask-app-movie-app.onrender.com/api/tv_search/${query}/`
+      );
     });
   } else {
     return await Axios.get(`${lastUrl}/${pgno}`).then((res) => {
@@ -94,10 +98,12 @@ export const search_movies_genre = async (
   if (query === "") return getMovies(setMoviesShows, setLastUrl, lastUrl);
   if (pgno === 1) {
     return await Axios.get(
-      `http://127.0.0.1:5000/api/movie_genre_search/${query}/${pgno}`
+      `https://flask-app-movie-app.onrender.com/api/movie_genre_search/${query}/${pgno}`
     ).then((res) => {
       setMoviesShows(res.data.results);
-      setLastUrl(`http://127.0.0.1:5000/api/movie_genre_search/${query}`);
+      setLastUrl(
+        `https://flask-app-movie-app.onrender.com/api/movie_genre_search/${query}`
+      );
     });
   } else {
     return await Axios.get(`${lastUrl}/${pgno}`).then((res) => {
@@ -116,10 +122,12 @@ export const search_tv_genre = async (
   if (query === "") return getTVShows(setMoviesShows, setLastUrl, lastUrl);
   if (pgno === 1) {
     return await Axios.get(
-      `http://127.0.0.1:5000/api/tv_genre_search/${query}/${pgno}`
+      `https://flask-app-movie-app.onrender.com/api/tv_genre_search/${query}/${pgno}`
     ).then((res) => {
       setMoviesShows(res.data.results);
-      setLastUrl(`http://127.0.0.1:5000/api/tv_genre_search/${query}`);
+      setLastUrl(
+        `https://flask-app-movie-app.onrender.com/api/tv_genre_search/${query}`
+      );
     });
   } else {
     return await Axios.get(`${lastUrl}/${pgno}`).then((res) => {
@@ -129,17 +137,17 @@ export const search_tv_genre = async (
 };
 
 export const getMoviesGenre = async (setGenre) => {
-  return await Axios.get(`http://127.0.0.1:5000/api/get_movies_genres`).then(
-    (res) => {
-      setGenre(res.data.genres);
-    }
-  );
+  return await Axios.get(
+    `https://flask-app-movie-app.onrender.com/api/get_movies_genres`
+  ).then((res) => {
+    setGenre(res.data.genres);
+  });
 };
 
 export const getTVShowsGenre = async (setGenre) => {
-  return await Axios.get(`http://127.0.0.1:5000/api/get_tv_genres`).then(
-    (res) => {
-      setGenre(res.data.genres);
-    }
-  );
+  return await Axios.get(
+    `https://flask-app-movie-app.onrender.com/api/get_tv_genres`
+  ).then((res) => {
+    setGenre(res.data.genres);
+  });
 };
